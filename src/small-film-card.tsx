75af634ac6fe278001d4ca3b6ab1d6b7f1a1041b
valdmity <vlsur@outlook.com>
2023-10-09 00:28:06 +0500

    small-film-card refactor

--- src/small-film-card.tsx
+++ src/small-film-card.tsx
@@ -3,20 +3,20 @@ type CardInfo = {
     imagePath: string;
 }
 
-function SmallFilmCard ({ title: name, imagePath }: CardInfo) {
+function SmallFilmCard({title, imagePath}: CardInfo) {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
         <img
           src={imagePath}
-          alt={name}
+          alt={title}
           width="{280}"
           height="{175}"
         />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">
-          {name}
+          {title}
         </a>
       </h3>
     </article>
